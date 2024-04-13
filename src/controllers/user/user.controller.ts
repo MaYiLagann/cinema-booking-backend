dc521d4b19077247a70f7031793313462dbbab59
MaYiLagann <lhwshy@naver.com>
2024-04-14 01:05:04 +0900

    Implement hashing user password on register

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -2,24 +2,31 @@ import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { UserService } from '../../services/user/user.service';
 import { UserRegisterRequestModel } from '../../models/user/user-register-request.model';
 import { Response } from 'express';
+import { hash } from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private configService: ConfigService,
+    private userService: UserService,
+  ) {}
 
   @Post()
-  register(
+  async register(
     @Body() body: UserRegisterRequestModel,
     @Res({ passthrough: true }) res: Response,
-  ): number {
+  ): Promise<number> {
     if (!body.email || !body.password) {
       res.status(HttpStatus.BAD_REQUEST);
       return 0;
     }
 
+    const salt = this.configService.get<string>('user.password_salt');
+
     const user = this.userService.create();
     user.email = body.email;
-    // user.password = body.password; // Todo: Hashing.
+    user.password = await hash(body.password, salt);
 
     return user.id;
   }

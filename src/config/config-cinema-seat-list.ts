14020f56ac05ebe7e1e854a94b9b4d63c7b38a50
MaYiLagann <lhwshy@naver.com>
2024-04-14 14:30:25 +0900

    Create config cinema seat list

---
+++ src/config/config-cinema-seat-list.ts
@@ -0,0 +1,158 @@
+export const ConfigCinemaSeatList = [
+  {
+    cinema_id: 1,
+    seat_id: 1,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 2,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 3,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 4,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 5,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 6,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 7,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 8,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 9,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 10,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 11,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 12,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 13,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 14,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 15,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 16,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 17,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 18,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 19,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 20,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 21,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 22,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 23,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 24,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 25,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 26,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 27,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 28,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 29,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 30,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 31,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 32,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 33,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 34,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 35,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 36,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 37,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 38,
+  },
+  {
+    cinema_id: 1,
+    seat_id: 39,
+  },
+];

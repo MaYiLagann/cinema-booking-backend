c004f169d6c454cb8d3246cd35d9152f1436d9be
MaYiLagann <lhwshy@naver.com>
2024-04-14 14:25:18 +0900

    Create controller cinema

--- src/controllers/controller.module.ts
+++ src/controllers/controller.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { ServiceModule } from '../services/service.module';
+import { CinemaController } from './cinema/cinema.controller';
 
 @Module({
   imports: [ServiceModule],
-  controllers: [UserController],
+  controllers: [UserController, CinemaController],
 })
 export class ControllerModule {}

375c8c9de54df335218bda3a812de8ab8a260c72
MaYiLagann <lhwshy@naver.com>
2024-04-14 14:24:22 +0900

    Create entity cinema ticket

---
+++ src/entities/cinema-ticket.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class CinemaTicket {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @Index()
+  cinema_id: number;
+
+  @Column()
+  @Index()
+  seat_id: string;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deleted_at: Date;
+}

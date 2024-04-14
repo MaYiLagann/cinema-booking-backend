8b5b387ed5978d5a3f3a24d444aee0df5e583e4b
MaYiLagann <lhwshy@naver.com>
2024-04-14 21:32:26 +0900

    Implement request ticket

--- src/controllers/app/app.controller.ts
+++ src/controllers/app/app.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { Public } from '../../decorators/public.decorator';
+import { CinemaTicketService } from '../../services/cinema-ticket/cinema-ticket.service';
+import { CinemaListResponseModel } from '../../models/cinema/cinema-list-response.model';
+import { ConfigCinemaSeatList } from '../../config/config-cinema-seat-list';
 
 @Controller()
 export class AppController {
+  constructor(private cinemaTicketService: CinemaTicketService) {}
+
   @Public()
   @Get()
   @Render('index')
@@ -20,7 +25,33 @@ export class AppController {
   @Public()
   @Get('booking')
   @Render('booking')
-  booking() {
-    return {};
+  async booking() {
+    const colLength = 12;
+    const tickets = await this.cinemaTicketService.getAll();
+    const list = ConfigCinemaSeatList.map((seat) => {
+      const idFromZero = seat.seat_id - 1;
+      const row = Math.trunc(idFromZero / colLength) + 1;
+      const col = Math.trunc(idFromZero % colLength) + 1;
+
+      const isEmpty = !tickets.some(
+        (x) => x.cinema_id === seat.cinema_id && x.seat_id === seat.seat_id,
+      );
+
+      return {
+        cinema_id: seat.cinema_id,
+        seat_id: seat.seat_id,
+        seat_name: `${this.toLetters(row)}${col}`,
+        is_empty: isEmpty,
+      };
+    });
+
+    return { list: list };
+  }
+
+  toLetters(num: number): string {
+    const mod = num % 26;
+    let pow = (num / 26) | 0;
+    const out = mod ? String.fromCharCode(64 + mod) : (--pow, 'Z');
+    return pow ? this.toLetters(pow) + out : out;
   }
 }
